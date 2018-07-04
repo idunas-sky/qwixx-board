94065e42502fb14264160dbed6cfa6e4c0bceba7
idunas-sky <fh@idunas.de>
2018-07-04 23:34:45 +0200

    Improved layout / colors / buttons ...

--- src/app/components/fault/fault.component.ts
+++ src/app/components/fault/fault.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-fault',
-    templateUrl: 'fault.component.html'
+    templateUrl: 'fault.component.html',
+    styleUrls: ['fault.component.scss']
 })
 export class FaultComponent {
 
